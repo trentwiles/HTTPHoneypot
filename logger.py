9102fc7d07a40ee435e30bb4b736e5de756723b0
trentwiles <wilest7@gmail.com>
2023-08-29 18:07:30 -0400

    emails

--- logger.py
+++ logger.py
@@ -1,11 +1,28 @@
 import requests
+import json
 from querycontacts import ContactFinder
-
+import sendgrid
+import os
+from sendgrid.helpers.mail import Mail, Email, To, Content
 
 qf = ContactFinder()
+
+def email(subject, to, data):
+    sg = sendgrid.SendGridAPIClient(api_key=json.loads(open('config.json').read())['key'])
+    from_email = Email(json.loads(open('config.json').read())['from'])
+    to_email = To(to)
+    content = Content("text/plain", data)
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.headers)
+    return
+
 
 def report(ip, path):
     abuse = qf.find(ip)
     if len(abuse) == 0:
         return None
-    return
+    for x in abuse:
+        print()
+    return ""
---
+++ tester.py
@@ -0,0 +1,3 @@
+import logger
+
+logger.email("hello", "test@example.com", "Hello,\n\nThis is a notice that one of your systems")
