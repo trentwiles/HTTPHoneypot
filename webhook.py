c6a1ef6190a1af934baa253c46dc20b49da42ba8
trentwiles <wilest7@gmail.com>
2023-08-30 11:35:39 -0400

    fix json

--- webhook.py
+++ webhook.py
@@ -2,4 +2,4 @@ import requests
 import json
 
 def webhook(message):
-    requests.post("https://ntfy.sh/" + json.loads(open("config.json").read()["webhook_path"]), data=message)
+    requests.post("https://ntfy.sh/" + json.loads(open("config.json").read())["webhook_path"], data=message)
